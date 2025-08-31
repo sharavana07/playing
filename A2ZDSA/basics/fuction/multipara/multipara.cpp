75372daf7c182e8ca9b274ed74650fe370d07f9b
sharavana07 <sharavanaragav@gmail.com>
2025-08-31 18:38:36 +0530

    fuctions multi para  pushing

---
+++ A2ZDSA/basics/fuction/multipara/multipara.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void agedeclaration(string name, int age){
+    cout<<"My name is "<<name<<" and my age is "<<age;
+}
+
+int main(){
+    agedeclaration("ragav",18);
+}
