e4382c8def7ba2fb69b10e50236c4cfde25a6ed1
sharavana07 <sharavanaragav@gmail.com>
2025-08-31 16:29:12 +0530

    fuctions pushing

---
+++ A2ZDSA/basics/fuction/simple/simple.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void birthday()
+{
+    cout << "Happy brithday to you ! :-) \\ *-*/ " << endl;
+}
+
+int main()
+{
+    birthday();
+    return 0;
+}
