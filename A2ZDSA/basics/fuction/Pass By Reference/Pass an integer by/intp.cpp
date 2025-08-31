b6838519d03a821c6a9b9df9789b49fe9304d343
sharavana07 <sharavanaragav@gmail.com>
2025-08-31 19:10:14 +0530

    Pass an integer by reference

---
+++ A2ZDSA/basics/fuction/Pass By Reference/Pass an integer by/intp.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void ChangeNum(int &num){
+    num = 18200;
+}
+
+int main(){
+    int value = 10;
+    cout<<value<<endl;
+    ChangeNum(value);
+    cout<<value<<endl;
+
+    return 0;
+}
