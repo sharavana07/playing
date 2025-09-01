bf9dd3c90c34e53338b3247d050011b64d2461ef
sharavana07 <sharavanaragav@gmail.com>
2025-09-01 22:55:26 +0530

    summa pushing about the left tri

---
+++ radom/partterns/left tri/sol.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+//to print a left based trinangle
+
+int main (){
+    
+    int n;
+
+    cout<<"Enter the number of lines the star to be printed: "<<endl;
+    cin>>n;
+
+    for(int i=1; i <= n; i++){
+
+        for(int j=1; j <= i; j++){
+
+            for(int l=n-1; l <=0 ; l++ ){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+
+        cout<<endl;
+    }
+
+
+    return 0;
+}
