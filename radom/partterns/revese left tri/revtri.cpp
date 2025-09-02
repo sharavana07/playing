dbd42cd5418e7d6da9c7a782e37ef51594d8b411
sharavana07 <sharavanaragav@gmail.com>
2025-09-02 09:13:24 +0530

    reversed tri

---
+++ radom/partterns/revese left tri/revtri.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+//to print the stars in revese order
+int main(){
+    int rows ;
+    cout<<"Enter the number of the number rows to be printed in the reversed triangle :"<<endl;
+    cin>>rows;
+
+    for(int i = rows; i >= 0; i--){
+        for(int j = 1; j <= i; j++){
+            cout<<"OOO";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
