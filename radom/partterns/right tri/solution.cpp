05042e10ccc75047994ee613a5e19ecf3a62ab5c
sharavana07 <sharavanaragav@gmail.com>
2025-09-02 08:50:41 +0530

    right tri

---
+++ radom/partterns/right tri/solution.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+//to print a * in right based triangle
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    for(int i=0; i <= n ; i++){
+
+        for(int j=0; j < n-i-1 ; j++){
+                cout<<" ";
+            }
+
+        for(int k=0; k <= i; k++){
+
+            
+            cout<<"*";
+        }
+
+        cout<<endl;
+    }
+    return 0;
+}
