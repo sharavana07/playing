c7b12bd6148245ac3f580bec99b496f711423358
sharavana07 <sharavanaragav@gmail.com>
2025-09-03 19:42:19 +0530

    summa

---
+++ radom/partterns/revese tri =/sol.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void revesecentertriangle(int n){
+
+    for(int i = n ; i > 0 ; i--){
+
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            cout << "*";
+        }
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+        // print stars
+        
+        cout << endl;
+    }
+}
+
+int main (){
+    revesecentertriangle(6);
+    return 0;
+}
