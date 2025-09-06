c80e78baaf40842133e0acad75e5054a3f7cbbfd
sharavana610-afk <sharavana610@gmail.com>
2025-09-06 19:31:24 +0530

    Patterns

---
+++ patters/1/one.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main (){
+
+    int n ;
+
+    cout<<"Enter the number of code need to print : "<<endl;
+    cin>>n;
+     for(int i = 0 ; i <= n ; i++){
+        for(int j = 0 ; j <= n ; j++){
+            cout<<"0";
+        }
+        cout<<endl;
+     }
+    return 0;
+}
