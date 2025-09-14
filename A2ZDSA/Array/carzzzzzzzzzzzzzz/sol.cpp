0f28c9a866a67d8b2d23a9d08fd96b2ce5625abd
Sharavana07 <Sharavanaragav@gmail.com>
2025-09-14 18:53:15 +0530

    pushing Carzzzz

---
+++ A2ZDSA/Array/carzzzzzzzzzzzzzz/sol.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    string car[6] = { "RollsRoyce" , "TATA" , "Benz" , "Tesla" , "KIA" , "Force"} ; 
+
+    cout<<car[3];
+    return 0;
+}
